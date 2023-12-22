4f90ccddf9f7dba43f384a5f8d6174b5373ea5d0
HseongH <hh4518@gmail.com>
2023-12-22 21:15:22 +0900

    폼(form) 바인딩

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,9 @@
 import { ref } from 'vue';
 
 const text = ref('');
-
-const onInput = (event) => (text.value = event.target.value);
 </script>
 
 <template>
-  <input :value="text" @input="onInput" placeholder="여기에 입력하기" />
+  <input v-model="text" placeholder="여기에 입력하기" />
   <p>{{ text }}</p>
 </template>
